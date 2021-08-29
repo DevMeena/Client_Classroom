6cb317df5f8080ea4d395eedab5f2625d084f604
DevMeena <68868864+DevMeena@users.noreply.github.com>
2021-08-30 00:44:28 +0530

    sign in routes

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const url = 'http://localhost:5000/home'
 
-export const signUp = (newUser) => axios.post(url,newUser)
+export const signUp = (newUser) => axios.post(url+"/signup",newUser)
+export const signIn = (User) => axios.post(url+"/signin",User)
 export const Dashboard = () => axios.get(url)
 
